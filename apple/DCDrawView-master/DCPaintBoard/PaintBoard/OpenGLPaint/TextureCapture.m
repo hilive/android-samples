760869087befd154c8dfa572a50bef25e134c030
cortxu <cortxu@tencent.com>
2021-04-28 13:37:38 +0800

    Update TextureCapture.m

--- apple/DCDrawView-master/DCPaintBoard/PaintBoard/OpenGLPaint/TextureCapture.m
+++ apple/DCDrawView-master/DCPaintBoard/PaintBoard/OpenGLPaint/TextureCapture.m
@@ -10,11 +10,11 @@
 #define CAPTURE_SHADER_SOURCE(NAME, ...) const char * const NAME = #__VA_ARGS__;
 
 CAPTURE_SHADER_SOURCE(CaptureVertexShader,
-                      attribute vec4 position;
+                      attribute vec3 position;
                       attribute vec2 texcoord;
                       varying vec2 v_texcoord;
                       void main() {
-  gl_Position=position;
+  gl_Position=vec4(position,1.0);
   v_texcoord=texcoord;
 }
                       );
@@ -31,6 +31,9 @@ CAPTURE_SHADER_SOURCE(CaptureFragmentsShader,
 @interface WAEJTextureCapture()
 @property (nonatomic, readonly) EAGLContext* context;
 @property (nonatomic, assign) GLuint frameBuffer;
+@property (nonatomic, assign) GLuint vao;
+@property (nonatomic, assign) GLuint vbo;
+@property (nonatomic, assign) GLuint ebo;
 @property (nonatomic, assign) GLuint glProgram;
 @property (nonatomic, assign) GLuint glPosition;
 @property (nonatomic, assign) GLuint glTextureCoords;
@@ -90,12 +93,42 @@ CAPTURE_SHADER_SOURCE(CaptureFragmentsShader,
     
     glUseProgram(_glProgram);
     _glPosition = glGetAttribLocation(_glProgram, "position");
-    glEnableVertexAttribArray(_glPosition);
     
     _glTextureCoords = glGetAttribLocation(_glProgram, "texcoord");
-    glEnableVertexAttribArray(_glTextureCoords);
     
     _glUniformTexture = glGetUniformLocation(_glProgram, "texSampler");
+    
+    glGenVertexArraysOES(1, &_vao);
+    glGenBuffers(1, &_vbo);
+    glGenBuffers(1, &_ebo);
+    
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+      // Positions    // Texture Coords
+      -1.0f,  -1.0f, 0.0f, 0.0f, 0.0f, // Bottom Left
+      1.0f, -1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
+      1.0f, 1.0f, 0.0f, 1.0f, 1.0f, // Top Right
+      -1.0f,  1.0f, 0.0f, 0.0f, 1.0f  // Top Left
+    };
+    
+    glBindVertexArrayOES(_vao);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const GLushort indices[] = { 0, 1, 2, 2, 3, 0 };
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    // Position attribute
+    glEnableVertexAttribArray(_glPosition);
+    glVertexAttribPointer(_glPosition, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+    
+    // TexCoord attribute
+    glEnableVertexAttribArray(_glTextureCoords);
+    glVertexAttribPointer(_glTextureCoords, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    
+    glBindVertexArrayOES(0);
     
     available = YES;
   } while (false);
@@ -114,6 +147,18 @@ CAPTURE_SHADER_SOURCE(CaptureFragmentsShader,
   
   if (_frameBuffer) {
     glDeleteRenderbuffers(1, &_frameBuffer);
+  }
+  
+  if (_vao) {
+    glDeleteVertexArraysOES(1, &_vao);
+  }
+
+  if (_vbo) {
+    glDeleteBuffers(1, &_vbo);
+  }
+
+  if (_ebo) {
+    glDeleteBuffers(1, &_ebo);
   }
   
   if (_glProgram) {
@@ -160,12 +205,12 @@ CAPTURE_SHADER_SOURCE(CaptureFragmentsShader,
   if (errCode != kCVReturnSuccess) {
     return NO;
   }
-
+  
   if (textureRef) {
     CFRelease(textureRef);
     textureRef = nil;
   }
-
+  
   errCode = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault,
                                                          texture_cache,
                                                          pixel_buffer,
@@ -201,67 +246,66 @@ CAPTURE_SHADER_SOURCE(CaptureFragmentsShader,
   if (!ready) {
     return NO;
   }
-
+  
+  glFlush();
+  
   GLint currProgram;
   glGetIntegerv(GL_CURRENT_PROGRAM, &currProgram);
-
+  
   GLint currTextureId;
   glGetIntegerv(GL_TEXTURE_BINDING_2D, &currTextureId);
   
   GLint currCubeMap;
-	glGetIntegerv(GL_TEXTURE_BINDING_CUBE_MAP, &currCubeMap);
- 
+  glGetIntegerv(GL_TEXTURE_BINDING_CUBE_MAP, &currCubeMap);
+  
   GLint currFrameBuffer;
   glGetIntegerv(GL_FRAMEBUFFER_BINDING, &currFrameBuffer);
-
+  
   GLint currRenderBuffer;
   glGetIntegerv(GL_RENDERBUFFER_BINDING, &currRenderBuffer);
-
+  
+  GLint currElementArrayBuffer;
+  glGetIntegerv(GL_ELEMENT_ARRAY_BUFFER_BINDING, &currElementArrayBuffer);
+  
+  GLint currArrayBuffer;
+  glGetIntegerv(GL_ARRAY_BUFFER_BINDING, &currArrayBuffer);
+  
+  GLint currVertexArray;
+  glGetIntegerv(GL_VERTEX_ARRAY_BINDING_OES, &currVertexArray);
+  
   glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
   glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, currRenderBuffer);
-
+  
+  glClear(GL_COLOR_BUFFER_BIT);
+  
   GLenum glError = glGetError();
-  glUseProgram(_glProgram);
-  glError = glGetError();
-  //设置物体坐标
-  GLfloat vertices[] = {
-    -1.0,-1.0,
-    1.0,-1.0,
-    -1.0,1.0,
-    1.0,1.0
-  };
-  glVertexAttribPointer(_glPosition, 2, GL_FLOAT, 0, 0, vertices);
-  glEnableVertexAttribArray(_glPosition);
-  glError = glGetError();
-  //设置纹理坐标
-  
-  GLfloat texCoords2[] = {
-    0,0,
-    1,0,
-    0,1,
-    1,1
-  };
-  
-  glVertexAttribPointer(_glTextureCoords, 2, GL_FLOAT, 0, 0, texCoords2);
-  glEnableVertexAttribArray(_glTextureCoords);
-  glError = glGetError();
-  glUniform1i(_glUniformTexture, 0);
-  glError = glGetError();
   glViewport(0, 0, bf_width, bf_height);
-
+  
+  glUseProgram(_glProgram);
+  
   glError = glGetError();
+  
+  glUniform1i(_glUniformTexture, 0);
+  
+  glBindVertexArrayOES(_vao);
+  
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, textureId);
-  glError = glGetError();
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-  [self.context presentRenderbuffer:GL_RENDERBUFFER];
   
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+  
+  glBindVertexArrayOES(0);
+  
+  [self.context presentRenderbuffer:GL_RENDERBUFFER];
   glError = glGetError();
-
-  glBindTexture(GL_TEXTURE_2D, currTextureId);
-  glBindTexture(GL_TEXTURE_CUBE_MAP, currCubeMap);
+  
+//  glBindTexture(GL_TEXTURE_2D, currTextureId);
+//  glBindTexture(GL_TEXTURE_CUBE_MAP, currCubeMap);
   glBindFramebuffer(GL_FRAMEBUFFER, currFrameBuffer);
-  glUseProgram(currProgram);
+//  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, currElementArrayBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, currArrayBuffer);
+//  glBindVertexArrayOES(currVertexArray);
+//  glUseProgram(currProgram);
   
   glError = glGetError();
   GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
